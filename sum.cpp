724b19e6ef3370380c07370a0bae1696bd9b413b
34-anish <luckyanish47@gmail.com>
2020-08-03 19:02:27 +0545

    added a new line

--- sum.cpp
+++ sum.cpp
@@ -6,4 +6,6 @@ int main(void)
     std::cout <<"I am sorry I forgot this" <<std::endl;
     std::cout <<"It's okay" <<std::endl;
     std::cout << "Oh I just forget semi colon my bad" <<std::endl;
+    std::cout << "aur kya chal raha hain?" <<std::endl;
+
 }
