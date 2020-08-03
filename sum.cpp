97772d25455448b672fc8219fe5c27f97259072f
34-anish <67496166+34-anish@users.noreply.github.com>
2020-08-03 16:51:49 +0545

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -5,4 +5,5 @@ int main(void)
     std::cout << "2+3= " <<2+3 <<std::endl;
     std::cout <<"I am sorry I forgot this" <<std::endl;
     std::cout <<"It's okay" <<std::endl;
+    std::cout << "Oh I just forget semi colon my bad" <<std::endl;
 }
